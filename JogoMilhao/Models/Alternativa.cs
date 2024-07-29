463344c197d3fbf74b78b4c70daa8e4c823f1682
Usuario <Usuario@CPD03-M04>
2024-07-29 15:50:05 -0300

    Adicionar arquivos de projeto.

---
+++ JogoMilhao/Models/Alternativa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoMilhao.Models
+{
+    public class Alternativa
+    {
+        public string Descricao { get; set; }
+        public bool correta{ get; set; }
+    }
+}
