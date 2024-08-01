2f6ba353225dd81ce3ba4dfbf27f144d43ac2af1
Usuario <Usuario@CPD01-M07>
2024-08-01 15:58:43 -0300

    ds

--- JogoMilhao/App.xaml.cs
+++ JogoMilhao/App.xaml.cs
@@ -1,7 +1,22 @@
-﻿namespace JogoMilhao
+﻿using JogoMilhao.Models;
+
+namespace JogoMilhao
 {
     public partial class App : Application
     {
+        List<Pergunta> Perguntas_faceis = new()
+        {
+            new Pergunta
+            {
+                Id = 1,
+                Enunciado = "Qual o nome dado a água em forma de gelo?" ,
+                new Alternativa { correta = false, Descricao = "Líquido" },
+                new Alternativa { correta = true, Descricao = "Sólido " },
+                new Alternativa { correta = false, Descricao = "Gasoso" },
+                new Alternativa { Correta = false, Descricao = "Vaporoso" }
+            }
+        }
+    };
         public App()
         {
             InitializeComponent();
